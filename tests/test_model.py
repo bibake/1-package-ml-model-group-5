5dbcd1a95d3b7be628a0ecf729ae43436d9faa4d
gowthamnair <56715034+gowthamnair@users.noreply.github.com>
2020-05-22 12:12:57 +0200

    Update test_model.py
    Basic framework for all functions inside model. not yet updated with proper paramters or assert conditions. "WIP" indicates type to be defined

--- tests/test_model.py
+++ tests/test_model.py
@@ -5,10 +5,38 @@ import pandas as pd
 import numpy as np
 import datetime as dt
 
-#column_names = ["date","weathersit", "temperature_C", "feeling_temperature_C","humidity","windspeed"]
-#error_df = pd.DataFrame(columns = column_names)
+#load_process_training_data pytest
+@pytest.mark.parametrize('df',[()])
+def test_load_process_training_data(df):
+    assert isinstance(model.process_training_data(df), pd.DataFrame),"Failed for given DataFrame"
 
-@pytest.mark.parametrize("dict,result",
+
+#train_and_persist pytest
+@pytest.mark.parametrize('rand_state,comp_fact',[(42,True)])
+def test_train_and_persist(rand_state,comp_fact):
+    assert isinstance(model.train_and_persist(rand_state,comp_fact),"WIP"),"Failed for given Parameters"
+
+
+#check_and_retrieve pytest
+@pytest.mark.parametrize('file,from_pack,rand_state,comp_fact',[(None,False,42,True)])
+def test_check_and_retrieve(file,from_pack,rand_state,comp_fact):
+    assert isinstance(model.check_and_retrieve(file,from_pack,rand_state,comp_fact),"WIP"),"Failed for given Parameters"
+
+
+#get_season pytest
+@pytest.mark.parametrize('date',[("2011-01-01")])
+def test_get_season(date):
+    assert isinstance(model.get_season(date),"WIP"),"Failed for given Date"
+
+
+#process_new_observation pytest
+@pytest.mark.parametrize('df',[()])
+def test_process_new_observation(df):
+    assert isinstance(model.process_new_observation(df),pd.Dataframe),"Failed for given DataFrame"
+
+
+#predict pytest
+@pytest.mark.parametrize('dict',
                          [(
                              {
                              "date": dt.datetime(2011, 1, 1, 10, 0, 0),
@@ -17,17 +45,8 @@ import datetime as dt
                              "feeling_temperature_C": 19.695,
                              "humidity": 76.0,
                              "windspeed": 16.9,
-                         },36)
+                         })
                              ])
 def test_predict(df,result):
-    assert  model.predict(dict)==result,"Failed for given Parameters"
-
-    """
-    try:
-        assert  model.predict(dict)==result,dict
-    except AssertionError as x:
-        df=pd.DataFrame.from_dict(x)
-        error_df=error_df.append(df)
-    """
-
-#error_df
+    assert  isinstance(model.predict(dict), int),"Does not produce Integer"
+    assert  isinstance(model.predict(dict), float),"Does not produce Float"
